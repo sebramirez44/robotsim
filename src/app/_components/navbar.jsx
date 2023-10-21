c0975bd1b5ba528bd1b002f1b696482461c3e9cc
sebastian <sebramirezcordero@gmail.com>
2023-10-21 18:03:29 -0500

    arregle issue de import styles

--- src/app/_components/navbar.jsx
+++ src/app/_components/navbar.jsx
@@ -1,20 +1,28 @@
 import Link from 'next/link'
 import './nav.css'
+import styles from './nav.css'
 export default function Navbar() {
     return (
-        <nav className="navbar">
-        {/* poner la image del logo */}
-        <div className="branding">
-          <div className="imgFluid">
-            <img src="/logoHacktx.png" width="100%"/>
-            <h1 className="imageTitle">RoboSim</h1>
-          </div>
+        <div className={styles.wrapper}>
+            <nav className="navbar" >
+            <link rel="stylesheet" href="./nav.css" />
+
+            {/* poner la image del logo */}
+            <div className="branding">
+                <div href="/" className="imgFluid">
+                    <img src="/logoHacktx.png" width="100%"/>
+                    <h1 className="imageTitle">RoboSim</h1>
+                </div>
+            
+            </div>
+            
+            
+            <ul>
+            <li><Link href="/" className="navItem">Home</Link></li>
+            <li><Link href="/login" className="navItem">Log In</Link></li>
+            </ul>
+            </nav>
         </div>
-        
-        <ul>
-          <li><Link href="/" className="navItem">Home</Link></li>
-          <li><Link href="/login" className="navItem">Log In</Link></li>
-        </ul>
-      </nav>
+       
     )
 }
